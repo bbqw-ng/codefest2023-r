5e09b5eec10c92b4cd50de9257232a45b07fa3dc
bw672 <bw672@drexel.edu>
2023-03-12 13:40:07 -0400

    minor changes in characters

--- workspace/forms.py
+++ workspace/forms.py
@@ -6,12 +6,12 @@ class teamBranch(FlaskForm):
     subjects = SelectField(u'subject', choices=['Team-Based'])
     team = SelectField(u'Type', choices=['Sprint Retrospective', 'Stand-up Meeting', 'Sprint Planning', '1-on-1 Meeting'])
     times = SelectField(u'Time', choices=['<10min','<30min','<1hr','<2hr'])
-    occurrence = SelectField(u'Occurrence', choices=['daily','weekly','monthly','annually'])
+    occurrence = SelectField(u'Occurrence', choices=['Daily','Weekly','Monthly','Annually'])
     submit = SubmitField('Submit')
 
 class genBranch(FlaskForm):
     subjects = SelectField(u'subject', choices=['General Announcements'])
     gen = SelectField(u'Type', choices=['Business Updates','Product Performance'])
     times = SelectField(u'Time', choices=['<10min','<30min','<1hr','<2hr'])
-    occurrence = SelectField(u'Occurrence', choices=['daily','weekly','monthly','annually'])
+    occurrence = SelectField(u'Occurrence', choices=['Daily','Weekly','Monthly','Annually'])
     submit = SubmitField('Submit')
